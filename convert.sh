ddd0fc94178c2e3b4b5782a5354cbdabac3e707e
Pleasedontbelong <pleasedontbelong@gmail.com>
2013-12-30 23:30:31 +0100

    Add convert.sh

--- convert.sh
+++ convert.sh
@@ -5,6 +5,6 @@ do
     filename=$(basename "$f")
 	extension="${filename##*.}"
 	filename="${filename%.*}"
-	new_name="_${filename}.avi"
+	new_name="final/_${filename}.avi"
 	ffmpeg -threads 2 -i "$f" -vcodec mpeg4 -acodec libmp3lame -ab 160 -ac 2 -ar 44100 "$new_name"
 done
